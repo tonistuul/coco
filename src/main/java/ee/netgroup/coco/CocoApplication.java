330c81afe01a84dd3bd26dff25da71df42c94d48
Tõnis Tuul <tonis.tuul@netgroup.ee>
2018-10-22 23:28:14 +0300

    Initialize project

---
+++ src/main/java/ee/netgroup/coco/CocoApplication.java
@@ -0,0 +1,12 @@
+package ee.netgroup.coco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CocoApplication.class, args);
+  }
+}
---
+++ src/test/java/ee/netgroup/coco/CocoApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.netgroup.coco;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CocoApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
