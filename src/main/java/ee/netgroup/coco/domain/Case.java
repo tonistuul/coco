e0c45eed5c8638e5355e6895f8ee32ad16950318
martlaus <mart.laus@netgroup.ee>
2018-10-26 19:00:23 +0300

    Merge claim and case

--- src/main/java/ee/netgroup/coco/domain/Case.java
+++ src/main/java/ee/netgroup/coco/domain/Case.java
@@ -4,15 +4,22 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Builder
 public class Case {
   private String status;
+  private String paymentStatus = "PAID";
   private String caseNumber;
   private String type;
   private LocalDate dateOfRegistration;
   private String judge;
   private String claimantId;
   private String defendantId;
-}
+  private List<String> files;
+
+  private String caseType;
+  private String description;
+  private String value;
+  private String fee;}
