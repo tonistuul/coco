7ddae90e7bf635aa72082ba747ec8eb4957cb56c
Tõnis Tuul <tonis.tuul@netgroup.ee>
2018-10-27 12:28:22 +0300

    Add backend build to run script

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+cd docker
+docker-compose down
+nohup docker-compose up &
+
+cd ../coco-react
+pkill npm
+git fetch --all
+git reset --hard origin/master
+npm install
+nohup npm start &
+
+cd ../coco-api/coco
+git fetch --all
+git reset --hard origin/master
 pkill java
-nohup java -jar -Dfile.encoding=UTF8 /opt/coco/coco-0.0.1-SNAPSHOT.jar &
+sh gradlew bootJar
+nohup java -jar -Dfile.encoding=UTF8 /build/libs/coco-0.0.1-SNAPSHOT.jar &
