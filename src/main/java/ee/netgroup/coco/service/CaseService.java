ba2b58b2cc38aa098b52af790a485a5f0d6fddc5
Karel Meriste <karel.meriste@netgroup.ee>
2020-02-09 12:51:12 +0200

    Add related legal entity cases to user cases view

--- src/main/java/ee/netgroup/coco/service/CaseService.java
+++ src/main/java/ee/netgroup/coco/service/CaseService.java
@@ -2,6 +2,7 @@ package ee.netgroup.coco.service;
 
 import ee.netgroup.coco.model.CourtCase;
 import ee.netgroup.coco.model.LegalEntity;
+import ee.netgroup.coco.model.Person;
 import ee.netgroup.coco.repository.CaseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -19,6 +21,7 @@ public class CaseService {
   public static final String CLOSED = "CLOSED";
   private final CaseRepository caseRepository;
   private final LegalEntityService legalEntityService;
+  private final PersonService personService;
 
   public CourtCase saveCase(CourtCase courtCase) {
     if (courtCase.getDateOfRegistration() != null) {
@@ -44,6 +47,16 @@ public class CaseService {
       .filter(c -> caseRepository.findByDefendantId(c.getRegistryCode()).stream().anyMatch(cc -> "PENDING".equals(cc.getStatus())))
       .findAny();
     return any.isPresent();
+  }
+
+  public Collection<CourtCase> getAllCourtCasesForPerson(String personId) {
+    Collection<CourtCase> personCases = caseRepository.findByDefendantIdOrClaimantId(personId,personId);
+    Collection<LegalEntity> companies = legalEntityService.findAllWherePersonOnBoard(personId);
+    personCases.addAll(companies.stream()
+      .map(c -> caseRepository.findByDefendantIdOrClaimantId(c.getRegistryCode(), c.getRegistryCode()))
+        .flatMap(Collection::stream)
+        .collect(Collectors.toList()));
+    return personCases;
   }
 
   public boolean hasClosedCasesWhereGuilty(String personId) {
